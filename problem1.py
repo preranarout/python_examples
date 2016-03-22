738ad9753dc43650dcfdc976105a5df4424c2edb
Prerana Rout <prerana786@gmail.com>
2016-03-22 19:26:35 -0400

    First problem creation.

---
+++ problem1.py
@@ -0,0 +1,3 @@
+# Problem description:
+# Take an input of two integers from the user and perform addition, subtraction, multiplication and division using different functions and print the output.
+# Also, make sure that your program can do the same functions for floating point values.
