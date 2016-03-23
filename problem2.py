c1f8ded09719da1e126e059062088b2ce0499f1b
preranarout <prerana786@gmail.com>
2016-03-23 10:20:33 -0400

    Problem 2 added.

---
+++ problem2.py
@@ -0,0 +1,4 @@
+# Create a new folder called problem2 and put all problem2 related files in that folder including this file.
+# Create a python file, say math_library.py, and add the functions for addition, subtraction, multiplication and division.
+# In this file, call the functions addition, subtraction, multiplication and division that you defined in the math_library.py.
+# Make sure that the program starts at the main function. Like in dinkelbach.py. Good job on the last one.
