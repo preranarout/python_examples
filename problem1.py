12928176573f962f639561c639c214cae89ec54c
Prerana Rout <prerana786@gmail.com>
2016-03-23 02:59:49 -0400

    Done

--- problem1.py
+++ problem1.py
@@ -1,3 +1,18 @@
 # Problem description:
 # Take an input of two integers from the user and perform addition, subtraction, multiplication and division using different functions and print the output.
 # Also, make sure that your program can do the same functions for floating point values.
+
+a, b = input("Enter two Integers: ")
+
+Addition = a + b
+print "The addition of a and b is:", Addition
+
+Subtraction = a - b
+print "The Subtraction of a and b is:", Subtraction
+
+Multiplication = a * b
+print "The Multiplication of a and b is:", Multiplication
+
+Division = a / b
+print "The Division of a and b is:", Division
+
