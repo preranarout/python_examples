bed95ec6d4f768a8fbcf26de34632a9f91520a5c
preranarout <prerana786@gmail.com>
2016-03-25 18:25:18 -0400

    Added problem3.py specifications.

---
+++ problem3.py
@@ -0,0 +1,13 @@
+# Good job on the last one.
+# Create a folder called problem3 and copy the math library that you created in problem2 
+# and add square root and square root functions. After that is done, copy this file problem3.py
+# into that folder and call the functions of the math library. Ask the user for an input using
+# command line arguments (argparse) to specify whether the user wants to add, subtract, 
+# multiply, divide, perform square root or find the square of a number using a parameter.
+#
+# Example Usage:
+#   $ python problem3.py --op_type 1 -n1 23 -n2 25
+#   
+# where op_type (1: add, 2: subtract, ... )
+#       n1 is the first number
+#       n2 is the second number (for square root and square, n2 need not be specified)
